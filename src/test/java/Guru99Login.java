1800f263a00a8b701aeaa1c560651fee91fadeb2
mnaeem <mnaeem@gmail.com>
2024-05-11 16:39:09 +0500

    Verification added
    Verification added

--- src/test/java/Guru99Login.java
+++ src/test/java/Guru99Login.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -28,7 +29,8 @@ public class Guru99Login {
         submitButton.click();
 
         //Verify Login
-
+        WebElement loginSuccessMessage = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[1]/td/h3"));
+        Assert.assertTrue(loginSuccessMessage.isDisplayed());
 
     }
 
