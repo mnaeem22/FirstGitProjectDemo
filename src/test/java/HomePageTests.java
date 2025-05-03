fa3f78438f172e28f7e1e9b19ecf8a4788c64559
Muhammad Naeem <mnaeem@gmail.com>
2025-05-03 02:50:17 -0700

    Update HomePageTests.java
    remote file change

--- src/test/java/HomePageTests.java
+++ src/test/java/HomePageTests.java
@@ -17,7 +17,7 @@ public class HomePageTests {
 
     @Test(priority = 1, groups = "Smoke Test")
     public void validateMercuryPageTitle(){
-
+        // This is my change in remote
         String actualMercuryPageTile = driver.getTitle();
         String expectedMercuryPageTitle = "Welcome: Mercury Tours";
         //Validate Page Title
